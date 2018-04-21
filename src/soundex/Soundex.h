fd6a26c3973cb3cbcaaf485b54114707d0507a22
gang <gangbj@163.com>
2018-04-22 00:00:14 +0800

    move to src

---
+++ src/soundex/Soundex.h
@@ -0,0 +1,38 @@
+#ifndef Soundex_h
+#define Soundex_h
+#include <string>
+#include <unordered_map>
+
+class Soundex {
+public:
+	static const size_t MaxCodeLength { 4 };
+
+	std::string encode(const std::string& word) const {
+		return zeroPad(head(word) + encodedDigits(word));
+	}
+
+private:
+	std::string head(const std::string& word) const {
+		return word.substr(0, 1);
+	}
+
+	std::string encodedDigits(const std::string& word) const {
+		if (word.length() > 1)
+			return encodedDigit(word[1]);
+		return "";
+	}
+
+	std::string encodedDigit(char letter) const {
+		const std::unordered_map<char, std::string> encodings { { 'b', "1" }, {
+				'c', "2" }, { 'd', "3" } };
+		return encodings.find(letter)->second;
+	}
+
+	std::string zeroPad(const std::string& word) const {
+		auto zerosNeeded = MaxCodeLength - word.length();
+		return word + std::string(zerosNeeded, '0');
+	}
+};
+
+#endif
+
