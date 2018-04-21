217af877a55982b4c8f5d01cda3a298ff88b00d5
gang <gangbj@163.com>
2018-04-21 21:45:17 +0800

    c2/10 add test fixture

--- soundex/SoundexTest.cpp
+++ soundex/SoundexTest.cpp
@@ -6,22 +6,30 @@ public:
    std::string encode(const std::string& word) const {
       return word+"000";
    }
+
+private:
+   std::string zeroPad(const std::string& word) const {
+      return word + "000";
+   }
 };
 
-#include "gmock/gmock.h"
-using ::testing::Eq;
 
-TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
+#include "gmock/gmock.h"
+
+using testing::Eq;
+
+class SoundexEncoding: public testing::Test {
+public:
    Soundex soundex;
-   
+};
+
+TEST_F(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
    auto encoded = soundex.encode("A");
 
    ASSERT_THAT(encoded, Eq("A000")); 
 }
 
-TEST(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
-   Soundex soundex;
-
+TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits) {
    auto encoded = soundex.encode("I");
 
    ASSERT_THAT(encoded, Eq("I000"));
