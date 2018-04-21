ca7eda4e9a54c6327fed683723903a43f4e96b23
gang <gangbj@163.com>
2018-04-21 20:40:28 +0800

    c2/6

--- soundex/SoundexTest.cpp
+++ soundex/SoundexTest.cpp
@@ -4,10 +4,9 @@ class Soundex
 {
 public:
    std::string encode(const std::string& word) const {
-      return "";
+      return "A";
    }
 };
-
 
 #include "gmock/gmock.h"
 
@@ -15,5 +14,7 @@ TEST(SoundexEncoding, RetainsSoleLetterOfOneLetterWord) {
    Soundex soundex;
    
    auto encoded = soundex.encode("A");
+
+   ASSERT_THAT(encoded, testing::Eq("A")); 
 }
 
