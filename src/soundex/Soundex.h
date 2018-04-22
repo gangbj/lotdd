e2db0ea03e923e29c2506a812607b399f8d26d87
gang <gangbj@163.com>
2018-04-22 12:00:52 +0800

    c2/27

--- src/soundex/Soundex.h
+++ src/soundex/Soundex.h
@@ -26,10 +26,14 @@ private:
       std::string encoding;
       for (auto letter:word)
       {
-         if (encoding.length() == MaxCodeLength - 1) break;
+         if (isComplete(encoding)) break;
 	 encoding += encodedDigit(letter);
       }
       return encoding;
+   }
+
+   bool isComplete (const std::string& encoding) const {
+      return encoding.length() == MaxCodeLength - 1;
    }
 
    std::string encodedDigit(char letter) const {
