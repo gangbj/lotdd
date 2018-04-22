ed355171a192784e709b42c60f3925ee3c271413
gang <gangbj@163.com>
2018-04-22 18:23:48 +0800

    c2/31 combines duplicate encodings

--- src/soundex/SoundexTest.cpp
+++ src/soundex/SoundexTest.cpp
@@ -34,3 +34,11 @@ TEST_F(SoundexEncoding, ReplacesMultipleConsonantsWithDigits) {
 TEST_F(SoundexEncoding, LimitsLengthToFourCharacters) {
    ASSERT_THAT(soundex.encode("Dcdlb").length(), Eq(4u));
 }
+
+TEST_F(SoundexEncoding, IgnoresVowelLikeLetters) {
+   ASSERT_THAT(soundex.encode("Baeiouhycdl"), Eq("B234"));
+}
+
+TEST_F(SoundexEncoding, CombinesDuplicateEncodings) {
+   ASSERT_THAT(soundex.encode("Abfcgdt"), Eq("A123"));
+}
